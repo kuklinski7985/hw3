553951318cabc360736a2e65a7a2c41146c88f16
Andrew Kuklinski <anku7985@colorado.edu>
2017-09-22 15:35:04 -0600

    hw3 problem 4, architecture data sizes for host machine and BBB

---
+++ print_arch_data.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+void main(){
+    printf("sizof char     : ");
+  printf("%d bytes\n",sizeof(char));
+  
+    printf("sizof int      : ");
+  printf("%d bytes\n",sizeof(int));
+  
+    printf("sizof float    : ");
+  printf("%d bytes\n",sizeof(float));
+
+    printf("sizof double   : ");
+  printf("%d bytes\n",sizeof(double));
+
+    printf("sizof short    : ");
+  printf("%d bytes\n",sizeof(short));
+
+    printf("sizof long     : ");
+  printf("%d bytes\n",sizeof(long));
+
+    printf("sizof long int : ");
+  printf("%d bytes\n",sizeof(long int));
+
+    printf("sizof long long: ");
+  printf("%d bytes\n",sizeof(long long));
+
+    printf("sizof int8_t   : ");
+  printf("%d bytes\n",sizeof(int8_t));
+
+    printf("sizof uint8_t  : ");
+  printf("%d bytes\n",sizeof(uint8_t));
+
+    printf("sizof uint16_t : ");
+  printf("%d bytes\n",sizeof(uint16_t));
+
+    printf("sizof uint32_t : ");
+  printf("%d bytes\n",sizeof(uint32_t));
+
+    printf("sizof char*    : ");
+  printf("%d bytes\n",sizeof(char*));
+
+    printf("sizof int*     : ");
+  printf("%d bytes\n",sizeof(int*));
+
+    printf("sizof float*   : ");
+  printf("%d bytes\n",sizeof(float*));
+
+    printf("sizof void*    : ");
+  printf("%d bytes\n",sizeof(void*));
+
+    printf("sizof void**   : ");
+  printf("%d bytes\n",sizeof(void**));
+
+    printf("sizof int8_t*  : ");
+  printf("%d bytes\n",sizeof(int8_t*));
+
+    printf("sizof int16_t* : ");
+  printf("%d bytes\n",sizeof(int16_t*));
+
+    printf("sizof int32_t* : ");
+  printf("%d bytes\n",sizeof(int32_t*));
+
+    printf("sizof size_t   : ");
+  printf("%d bytes\n",sizeof(size_t));
+
+}
